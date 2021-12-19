15853d3635491ea061574020ad333532b4a70fbb
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2021-12-19 20:39:01 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -24,7 +24,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 REPLY_MARKUP = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton("🚀 Deploy Yours Now! 😍", url="https://github.com/XMYSTERlOUSX/mega-link-downloader-bot")],
+        [InlineKeyboardButton("HB4All", url="https://t.me/HB4All")],
     ]
 )
 
